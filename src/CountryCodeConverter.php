5d9de0a0d517c57d56753ef8c3767c11e535ed03
jmf3658 <jfullmer@austin.utexas.edu>
2025-10-20 10:31:48 -0700

    Avoid PHP 8.4 deprecation on str_getcsv

--- src/CountryCodeConverter.php
+++ src/CountryCodeConverter.php
@@ -19,14 +19,14 @@ class CountryCodeConverter {
    * Main converter method.
    *
    * @param string $input
-   *    A country name or 2- or 3-digit code.
+   *   A country name or 2- or 3-digit code.
    * @param string $output
-   *    The "type" of output desired (e.g., 2-digit code).
+   *   The "type" of output desired (e.g., 2-digit code).
    *
    * @return string
-   *    The desired output
+   *   The desired output
    */
-  public static function convert($input = '', $output = '') {
+  public static function convert($input = '', $output = 'two-digit') {
     $output_map = [
       'name' => 0,
       'two-digit' => 1,
@@ -34,25 +34,15 @@ class CountryCodeConverter {
       'numeric' => 3,
     ];
     $path = __DIR__ . '/data/countries.csv';
-    $data = array_map('str_getcsv', file($path));
-    foreach ($data as $key => $set) {
-      if (in_array($input, $set)) {
-        if ($output && array_key_exists($output, $output_map)) {
-          $type = $output_map[$output];
-          return $set[$type];
-        }
-        else {
-          switch ($input) {
-            case $set[0]:
-              return $set[1];
-
-            default:
-              return $set[0];
-          }
+    if (($handle = fopen($path, "r")) !== FALSE) {
+      while (($data = fgetcsv($handle, 1000, ",", '"', "")) !== FALSE) {
+        if (in_array($input, $data)) {
+          $output_format = $output_map[$output];
+          return $data[$output_format];
         }
       }
+      fclose($handle);
     }
-    // If no matches, return the original user input.
     return $input;
   }
 
